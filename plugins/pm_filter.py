bd9d3f81604c404f1c6031d763f9c5bea6c77186
joker153 <77005994+joker153@users.noreply.github.com>
2022-05-14 16:22:00 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2,7 +2,7 @@
 import asyncio
 import re
 import ast
-import t.mp3
+import upload
 
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
